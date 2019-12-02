3bb326bda1ac8f36f9c59fe2d843e25f32b37043
Hannes Sörensson <hannes.sorensson@gmail.com>
2019-12-02 19:37:53 +0100

    Solution to day 1, part 2

--- day1/apa.exs
+++ day1/apa.exs
@@ -2,11 +2,29 @@ defmodule DayOne do
   def fuel(mass) do
     div(mass, 3) -2
   end
+  def fuelfuel(mass) do
+    _fuelfuel(mass, 0)
+  end
+  def _fuelfuel(mass, acc) do
+    fuel1 = div(mass, 3) -2
+    case fuel1 do
+      x when x <= 0 ->
+        acc
+      _ ->
+        _fuelfuel(fuel1, acc + fuel1)
+    end
+  end
   def fueltotal([], acc) do
     acc
   end
   def fueltotal([m|ms], acc) do
     fueltotal(ms, acc + fuel(m))
+  end
+  def fuelfueltotal([], acc) do
+    acc
+  end
+  def fuelfueltotal([m|ms], acc) do
+    fuelfueltotal(ms, acc + fuelfuel(m))
   end
 end
 
@@ -19,4 +37,6 @@ ms = Enum.map(splits,
 ms = Enum.map(ms, fn ({m,_}) -> m end)
 #              fn ({m,_}) -> m end)
 answer = DayOne.fueltotal(ms, 0)
-IO.puts("Answer is #{answer}")
+IO.puts("Answer 1 is #{answer}")
+answer2 = DayOne.fuelfueltotal(ms, 0)
+IO.puts("Answer 2 is #{answer2}")
