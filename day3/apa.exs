2b8c02c484e2a303f7e970540625d42dbe3de8a4
Hannes Sörensson <hannes.sorensson@gmail.com>
2019-12-03 22:43:19 +0100

    Solution to day 3, part 2
    Some of these functions have pretty terrible names.

--- day3/apa.exs
+++ day3/apa.exs
@@ -36,10 +36,30 @@ defmodule DayThree do
       {:right, length}  -> goright(length, pos)
     end
   end
-  def goup(len, {x,y}), do: {Enum.map(0..len, fn i    -> {x,y-i} end), {x,y-len}}
-  def godown(len, {x,y}), do: {Enum.map(0..len, fn i  -> {x,y+i} end), {x,y+len}}
-  def goleft(len, {x,y}), do: {Enum.map(0..len, fn i  -> {x-i,y} end), {x-len,y}}
-  def goright(len, {x,y}), do: {Enum.map(0..len, fn i -> {x+i,y} end), {x+len,y}}
+  def getcoordsposandsteps(stretch, pos, steps) do
+    case stretch do
+      {:up, length}     -> goupsteps(length, pos, steps)
+      {:down, length}   -> godownsteps(length, pos, steps)
+      {:left, length}   -> goleftsteps(length, pos, steps)
+      {:right, length}  -> gorightsteps(length, pos, steps)
+    end
+  end
+  def goup(len, {x,y}), do: {Enum.map(1..len, fn i    -> {x,y-i} end), {x,y-len}}
+  def godown(len, {x,y}), do: {Enum.map(1..len, fn i  -> {x,y+i} end), {x,y+len}}
+  def goleft(len, {x,y}), do: {Enum.map(1..len, fn i  -> {x-i,y} end), {x-len,y}}
+  def goright(len, {x,y}), do: {Enum.map(1..len, fn i -> {x+i,y} end), {x+len,y}}
+  def goupsteps(len, {x,y}, steps) do
+    {Enum.map(1..len, fn i -> {x,y-i,steps+i} end), {x,y-len},steps+len}
+  end
+  def godownsteps(len, {x,y}, steps) do
+    {Enum.map(1..len, fn i  -> {x,y+i,steps+i} end), {x,y+len},steps+len}
+  end
+  def goleftsteps(len, {x,y}, steps) do
+    {Enum.map(1..len, fn i  -> {x-i,y,steps+i} end), {x-len,y},steps+len}
+  end
+  def gorightsteps(len, {x,y}, steps) do
+    {Enum.map(1..len, fn i -> {x+i,y,steps+i} end), {x+len,y},steps+len}
+  end
   def traverse([], _) do
     []
   end
@@ -47,25 +67,32 @@ defmodule DayThree do
     {coords, newpos} = getcoordsandpos(stretch, pos)
     coords ++ traverse(ss, newpos)
   end
+  def traversewithsteps([], _, _) do
+    []
+  end
+  def traversewithsteps([stretch|ss], pos, steps) do
+    {coords, newpos, newsteps} = getcoordsposandsteps(stretch, pos, steps)
+    coords ++ traversewithsteps(ss, newpos, newsteps)
+  end
   def work({wire1, wire2}) do
     {Enum.sort(traverse(wire1, {0,0})), Enum.sort(traverse(wire2, {0,0}))}
   end
-  def findmatch([], [], lowest) do
-    lowest
+  def work2({wire1, wire2}) do
+    {Enum.sort(traversewithsteps(wire1, {0,0}, 0), &sortfun/2),
+     Enum.sort(traversewithsteps(wire2, {0,0}, 0), &sortfun/2)}
   end
-  def findmatch(_, [], lowest) do
-    lowest
+  def sortfun({ax,ay,_},{bx,by,_}) do
+    {ax,ay} <= {bx,by}
   end
-  def findmatch([], _, lowest) do
-    lowest
-  end
+  def findmatch([], [], lowest), do: lowest
+  def findmatch(_, [], lowest), do: lowest
+  def findmatch([], _, lowest), do: lowest
   def findmatch([{0,0}|as], bs, lowest) do
     findmatch(as,bs, lowest)
   end
   def findmatch(as, [{0,0}|bs], lowest) do
     findmatch(as,bs, lowest)
   end
-
   def findmatch(ass,bss,lowest) do
     [a|as] = ass
     [b|bs] = bss
@@ -80,22 +107,44 @@ defmodule DayThree do
                end
     end
   end
+  def findmatchst([], [], lowest), do: lowest
+  def findmatchst(_, [], lowest), do: lowest
+  def findmatchst([], _, lowest), do: lowest
+  def findmatchst([{0,0,0}|as], bs, lowest) do
+    findmatchst(as,bs, lowest)
+  end
+  def findmatchst(as, [{0,0,0}|bs], lowest) do
+    findmatchst(as,bs, lowest)
+  end
+
+  def findmatchst(ass,bss,lowest) do
+    [a|as] = ass
+    [b|bs] = bss
+    {ax,ay,ast} = a
+    {bx,by,bst} = b
+    cond do
+      {ax,ay} < {bx,by} -> findmatchst(as, bss, lowest)
+      {ax,ay} > {bx,by} -> findmatchst(ass, bs, lowest)
+      true              -> cand = ast + bst
+                           case lowest do
+                             nil -> findmatchst(as,bs,cand)
+                             _   -> findmatchst(as,bs,min(lowest,cand))
+                           end
+    end
+  end
   def answerone() do
     getinput()|>workone()
   end
   def workone(input) do
-    getdata(input)|>
-    parsedata()|>
-    work()|>
+    getdata(input)|>parsedata()|>work()|>
     (fn({a,b})->findmatch(a,b,nil)end).()
+  end
+  def answertwo() do
+    getinput()|>worktwo()
+  end
+  def worktwo(input) do
+    getdata(input)|>parsedata()|>work2()|>
+    (fn({a,b})->findmatchst(a,b,nil)end).()
   end
 end
 
-
-#ints = Enum.map(splits, &Integer.parse/1)
-#ints = Enum.map(ints, fn ({m,_}) -> m end)
-#ints = List.to_tuple(ints)
-#answer = DayTwo.execute(part1ints, 0)
-#IO.puts("Answer 1 is #{answer}")
-#answer2 = DayTwo.loop(ints, 0, 0)
-#IO.puts("Answer 2 is #{answer2}")
